553d8fe044f0cbd7b9eba16e19ff82cba5c212b3
Kauan Santos Lima <kauansantosl2006@gmail.com>
2024-11-28 14:30:29 +0000

    T001: refact - Criação da estrutura do formulário da tela CadastraTarefas

--- lista-tarefas-dados-locais/src/app/cadastra-tarefas/cadastra-tarefas.page.ts
+++ lista-tarefas-dados-locais/src/app/cadastra-tarefas/cadastra-tarefas.page.ts
@@ -1,16 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+  Component, 
+  OnInit
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { ReactiveFormsModule } from '@angular/forms';
+import { 
+  IonContent,
+  IonHeader, 
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonTextarea,
+  IonDatetime,
+  IonButton,
+  IonSelect,
+  IonSelectOption,
+  IonRadioGroup,
+  IonRadio
+} from '@ionic/angular/standalone';
+
+interface ComponenteDocente {
+  componente: string;
+  docente: string;
+}
 
 @Component({
   selector: 'app-cadastra-tarefas',
   templateUrl: './cadastra-tarefas.page.html',
   styleUrls: ['./cadastra-tarefas.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    
+    ReactiveFormsModule,
+
+    IonContent,
+    IonHeader, 
+    IonTitle,
+    IonToolbar,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonTextarea,
+    IonDatetime,
+    IonButton,
+    IonSelect,
+    IonSelectOption,
+    IonRadioGroup,
+    IonRadio
+  ]
 })
 export class CadastraTarefasPage implements OnInit {
+  title: string = 'Cadastro de tarefas';
+
+  componentes: ComponenteDocente[] =[
+    {
+      componente: "PAM I",
+      docente: "Danilo"
+    },
+    {
+      componente: "PW II",
+      docente: "Ana Paula"
+    },
+    {
+      componente: "BD II",
+      docente: "Vinicius"
+    },
+    {
+      componente: "DS I",
+      docente: "Aquiles"
+    },
+    {
+      componente: "ECO",
+      docente: "Carlos Cruz"
+    }
+  ]
 
   constructor() { }
 
