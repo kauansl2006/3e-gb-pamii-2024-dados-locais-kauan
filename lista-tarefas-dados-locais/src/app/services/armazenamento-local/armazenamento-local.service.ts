61afdb19b6290b47471fb15f7250086dcaefa85c
Kauan Santos Lima <kauansantosl2006@gmail.com>
2024-11-29 11:11:30 +0000

    T002: feat - Criação dos métodos para salvar e retornar dados do LocalStorage

--- lista-tarefas-dados-locais/src/app/services/armazenamento-local/armazenamento-local.service.ts
+++ lista-tarefas-dados-locais/src/app/services/armazenamento-local/armazenamento-local.service.ts
@@ -1,9 +1,61 @@
 import { Injectable } from '@angular/core';
+import { ComponenteDocente } from 'src/app/models/componente-docente';
+import { Tarefa } from 'src/app/models/tarefa';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArmazenamentoLocalService {
+  private readonly STORAGE: Storage | undefined;
 
-  constructor() { }
+  constructor() {
+    this.STORAGE =  window.localStorage;
+  }
+
+  private generateId(): number {
+    let id = 0;
+
+    do {
+      id =  new Date().getMilliseconds() * Math.round(Math.random() * 1000);
+    } while (
+      this.STORAGE?.getItem(id.toString())
+    );
+
+    return id;
+  }
+
+  private dataToTarefa(data: any): Tarefa {
+    return {
+      id: this.generateId(),
+      titulo: data.titulo.value,
+      componente: data.componente.value as ComponenteDocente,
+      descricao: data.descricao.value,
+      data_aviso: data.data_aviso.value,
+      data_entrega: data.data_entrega.value,
+      individual: data.individual.value,
+    }
+  }
+
+  save(data: any) {
+    const tarefa: Tarefa = this.dataToTarefa(data);
+
+    this.STORAGE?.setItem(tarefa.id.toString(), JSON.stringify(tarefa));
+  }
+
+  getAll(): Tarefa[] {
+    const tarefas: Tarefa[] | null = [];
+
+    if(this.STORAGE) {
+      for(let index = 0; index < this.STORAGE.length; index++) {
+        const tarefaId: string = this.STORAGE.key(index) ?? '';
+        const tarefa: string | null = this.STORAGE.getItem(tarefaId);
+
+        if(tarefa) {
+          tarefas.push(JSON.parse(tarefa));
+        }
+      }
+    }
+
+    return tarefas;
+  }
 }
