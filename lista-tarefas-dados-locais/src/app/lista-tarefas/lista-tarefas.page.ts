9d29fd44fe3616e475efaf1afd649b6c372bcad2
Kauan Santos Lima <kauansantosl2006@gmail.com>
2024-12-03 23:29:09 +0000

    T002: feat - Exibindo tarefas na tela utilizando as diretivas ngIf e ngFor

--- lista-tarefas-dados-locais/src/app/lista-tarefas/lista-tarefas.page.ts
+++ lista-tarefas-dados-locais/src/app/lista-tarefas/lista-tarefas.page.ts
@@ -1,21 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+  Component,
+  OnInit
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { 
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonItemSliding,
+  IonItemOptions,
+  IonItemOption,
+  IonLabel
+} from '@ionic/angular/standalone';
+import { Tarefa } from '../models/tarefa';
+import { ArmazenamentoLocalService } from '../services/armazenamento-local/armazenamento-local.service';
 
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.page.html',
   styleUrls: ['./lista-tarefas.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonList,
+    IonItem,
+    IonItemSliding,
+    IonItemOptions,
+    IonItemOption,
+    IonLabel
+  ]
 })
 export class ListaTarefasPage implements OnInit {
   titulo: string = 'lista-tarefas';
+  tarefas: Tarefa[] = [];
 
-  constructor() { }
+  constructor(
+    private readonly armLocalServ: ArmazenamentoLocalService
+  ) { }
 
   ngOnInit() {
+    this.tarefas = this.armLocalServ.getAll();
+    console.log(this.tarefas);
   }
 
 }
